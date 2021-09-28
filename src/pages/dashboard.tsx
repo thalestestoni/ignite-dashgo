8a03452ee6f02879474952d1b2ca32e38b470d63
Thales Testoni <thalestestoni@gmail.com>
2021-09-28 20:35:38 -0300

    Types to ApexOptions

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,12 +2,13 @@ import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 import { Header } from "../components/Header";
 import { Sidebar } from '../components/Siderbar';
+import { ApexOptions } from 'apexcharts';
 
 const Chart = dynamic(() => import('react-apexcharts'), {
   ssr: false
 });
 
-const options = {
+const options: ApexOptions = {
   chart: {
     toolbar: {
       show: false
