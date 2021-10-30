3b68c640ae4633889e9aa8c295e56f55d1c5c913
Thales Testoni <thalestestoni@gmail.com>
2021-10-30 10:09:53 -0300

    Miragejs factories and seeds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
 
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
